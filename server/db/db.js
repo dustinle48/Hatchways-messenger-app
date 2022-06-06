48f4f5470ca5fca2cd645a77089fb337698dbe24
dustinle48 <ducthinh481994@gmail.com>
2022-06-06 16:30:25 -0400

    Change db.js initialize Sequelize instance back to using URI like original code

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,10 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_NAME,process.env.DATABASE_USERNAME,process.env.DATABASE_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres'
-}
+const db = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost:5432/messenger",
+  {
+    logging: false,
+  }
 );
 
 module.exports = db;
